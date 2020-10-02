2759cf95d935976468d93870a1068235c47542a9
Yoryi Eduardo Abreu Pacheco <yoryi.pacheco@gmail.com>
2020-10-02 20:26:27 -0300

    Last for deployment

---
+++ myapp/build/precache-manifest.bf10f306de6ea1e06bf1161049bb6010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a7c7cfa636ec8b7cc34d369fad9040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4d7144b3b313cea275",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "5af11085d8503dc68af3",
+    "url": "/static/css/main.ff3d03cd.chunk.css"
+  },
+  {
+    "revision": "0c4d7144b3b313cea275",
+    "url": "/static/js/2.9b261793.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9b261793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af11085d8503dc68af3",
+    "url": "/static/js/main.868cb8e2.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
